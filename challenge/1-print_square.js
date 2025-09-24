ffda9f4339b6fbb8f025792ef639ab1e40bae554
C-Florian <florianchereau@hotmail.fr>
2025-09-24 10:45:53 +0200

    Fix Task 1: print square with 'X' and parseInt base 10; align with checker output

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 /**
- * Print a square with the character '#'
- * The size of the square is given as the first command line argument
+ * Print a square with the character "X"
+ * The size of the square is the first command line argument
  */
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./1-print_square.js <size>');
-  process.exit(1);
-}
+const size = parseInt(process.argv[2], 10);
 
-const size = parseInt(process.argv[2], 10); // ✅ base 10 obligatoire
-
-if (isNaN(size)) {
-  console.log('Size must be a number');
-  process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-  console.log('#'.repeat(size));
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
