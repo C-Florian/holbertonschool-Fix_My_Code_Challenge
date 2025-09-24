3fed3274c72e5de4bd7ac0b2403dc2e3df75706c
C-Florian <florianchereau@hotmail.fr>
2025-09-24 10:49:28 +0200

    Task 1: print square with 'X' and show 'Missing size' if arg is not a number

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 /**
  * Print a square with the character "X"
- * The size of the square is the first command line argument
+ * The size is the first CLI argument
  */
 
 const size = parseInt(process.argv[2], 10);
 
-if (!isNaN(size)) {
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
